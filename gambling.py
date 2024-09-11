a9697f54664203ad8a584019db628303472043dc
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2024-09-11 16:05:16 +0300

    idfk anymore

--- gambling.py
+++ gambling.py
@@ -1,8 +1,8 @@
 class Grieziens():
     def __int__(spin, balva, maksa, macins):
-        spin.prize = balva
-        spin.cost = maksa
-        spin.wallet = macins
+        spin.prize = int(balva)
+        spin.cost = int(maksa)
+        spin.wallet = int(macins)
     def play(spin):
         spin.wallet -=1
     def uzvara(spin):
@@ -16,22 +16,24 @@ symbols = [1, 2, 3]
 def spin():
     return random.choice(symbols)
 
-roulette = Grieziens(2, 1, 10)
-
+roulette = Grieziens()
+roulette.prize = 2
+roulette.cost = 1
+roulette.wallet = 10
 while i < 2:
     yes = input("do you want to gamble? Y/N  ")
     if yes == "N":
         i=3
-        print(roulette.wallet + " credits left, GG!")   
+        print(roulette.wallet, "credits left, GG!")
     if yes == "Y":
         roulette.play()
-        if roulette.Wcondition == spin:
+        if roulette.WinCon == spin:
             print("You win!")
             roulette.uzvara()
-            print(roulette.wallet + " credits remaining. ")
+            print(roulette.wallet, " credits remaining. ")
         else:
             print("You lose!")
-            print(roulette.wallet + " credits remaining. ")
+            print(roulette.wallet, " credits remaining. ")
 
 
 
